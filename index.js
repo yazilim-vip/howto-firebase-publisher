66412542298e00d1814906c67d24ef0ad1b1f1f4
Emre Sen <maemresen@yazilim.vip>
2020-10-15 22:39:14 +0300

    feat: script added

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const generateHowtoAsync = require("@yazilimvip/howto-script/build/service/HowtoServiceAsync")
+  .default;
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+const firebase = require("firebase/app");
+
+// Add the Firebase services that you want to use
+require("firebase/database");
+require("firebase/auth");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const genericErrorHandler = (error) => {
+    console.log("errorCode", error.code);
+    console.log("errorMessage", error.message);
+  };
+
+  const API_KEY = core.getInput("api-key");
+  const DATABASE_URL = core.getInput("database-url");
+  const PROJECT_ID = core.getInput("project-id");
+
+  const USERNAME = core.getInput("username");
+  const PASSWORD = core.getInput("password");
+
+  const firebaseConfig = {
+    apiKey: API_KEY,
+    databaseURL: DATABASE_URL,
+    projectId: PROJECT_ID,
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(USERNAME, PASSWORD)
+    .catch(genericErrorHandler)
+    .then(() => {
+      generateHowtoAsync(`${__dirname}/../howto`).then((result) => {
+        firebase
+          .database()
+          .ref("howto")
+          .set(JSON.stringify(result))
+          .catch(genericErrorHandler)
+          .then(() => {
+            console.log("Operation Completed!");
+            firebase
+              .auth()
+              .signOut()
+              .catch(genericErrorHandler)
+              .then(() => {
+                console.log("Sign Out Success!");
+                process.exit(0);
+              });
+          });
+      });
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
